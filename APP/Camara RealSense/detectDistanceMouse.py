11fb1ae112a3f3e1a9d0144ccfb7b709a0722ddb
cantgetpastmesa <f.mesan@uniandes.edu.co>
2023-02-18 03:15:09 -0500

    Organización de archivos

--- APP/Camara RealSense/detectDistanceMouse.py
+++ APP/Camara RealSense/detectDistanceMouse.py
@@ -8,17 +8,17 @@ def show_distance(event, x, y, args, params):
     global point
     point = (x, y)
 
-# Inicia la cámara Intel Realsense
+# Initialize Camera Intel Realsense
 dc = DepthCamera()
 
-# Crea una instacia del mouse para insertarla en la imágen
+# Create mouse event
 cv2.namedWindow("Color frame")
 cv2.setMouseCallback("Color frame", show_distance)
 
 while True:
     ret, depth_frame, color_frame = dc.get_frame()
 
-    # Muestra la distancia del punto especificado 
+    # Show distance for a specific point
     cv2.circle(color_frame, point, 4, (0, 0, 255))
     distance = depth_frame[point[1], point[0]]
 
@@ -27,6 +27,5 @@ while True:
     cv2.imshow("depth frame", depth_frame)
     cv2.imshow("Color frame", color_frame)
     key = cv2.waitKey(1)
-    #La tecla "ESC" rompe el ciclo de visión
     if key == 27:
         break
